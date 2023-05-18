e996b1501b4729481137b482170ef84c81adc414
Harrsh Patel <dev.harrsh@gmail.com>
2023-05-18 17:00:41 +0530

    Changed script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,12 +16,3 @@ echo "|----------------------------------|"
 echo "|        Generating the build        |"
 echo "|----------------------------------|"
 yarn build
-
-echo "|------------------------------|"
-echo "|        Restart server        |"
-echo "|------------------------------|"
-pm2 restart secure-sure-website --update-env
-
-echo "|-------------------------------------|"
-echo "|        Deployment successful        |"
-echo "|-------------------------------------|"
