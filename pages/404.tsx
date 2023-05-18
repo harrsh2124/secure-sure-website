b57ba6e2feebf774126bfefcdc345978cf0de551
Harrsh Patel <dev.harrsh@gmail.com>
2023-05-18 13:00:09 +0530

    Tweaks

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Fragment } from 'react';
-import PageNotFoundPng from '../public/siteImages/PageNotFound.png';
+import PageNotFoundPng from '../public/siteImages/PageNotFound.svg';
 
 const PageNotFound = () => {
     return (
@@ -12,13 +12,17 @@ const PageNotFound = () => {
             </Head>
 
             <div className="flex items-center justify-evenly md:justify-center flex-col-reverse md:flex-row w-full">
-                <Image
-                    src={PageNotFoundPng}
-                    alt="404, page not found!!"
-                    width={800}
-                />
+                <div className="relative w-full md:w-1/2 h-96">
+                    <Image
+                        src={PageNotFoundPng}
+                        alt="404, page not found!!"
+                        quality={100}
+                        fill
+                        priority
+                    />
+                </div>
 
-                <div className="flex flex-col">
+                <div className="flex flex-col w-full md:w-1/2">
                     <div>
                         <p className="text-5xl">Oops!!</p>
 
