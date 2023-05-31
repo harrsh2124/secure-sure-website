0587f4dc022d40a1fe39a31adca8d39bbba4812e
Harrsh Patel <dev@harrsh.com>
2023-05-31 23:16:17 +0530

    Project setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Space_Grotesk } from 'next/font/google';
+
+import '@/styles/globals.css';
+
+const font = Space_Grotesk({
+    preload: true,
+    subsets: ['latin', 'latin-ext'],
+    weight: ['300', '400', '500', '600', '700']
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <main className={`${font.className}`}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
