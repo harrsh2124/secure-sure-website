4057f8548ec2e3862d6cdca1146b01fd952d90a9
Harrsh Patel <dev@harrsh.com>
2023-06-18 09:57:49 +0530

    Added form validation

--- components/AboutUs/ContactUs.tsx
+++ components/AboutUs/ContactUs.tsx
@@ -1,15 +1,66 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import _ from 'lodash';
 import Image from 'next/image';
-import { FormEvent } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import validator from 'validator';
+import { z } from 'zod';
 import ArrowRight from '../../public/assets/AboutUs/arrowRight.svg';
 import Map from './Map';
 
+const schema = z.object({
+    firstName: z
+        .string({
+            required_error: 'First name is required.',
+            invalid_type_error: 'First name is required.'
+        })
+        .nonempty('First name is required.'),
+    lastName: z
+        .string({
+            required_error: 'Last name is required.',
+            invalid_type_error: 'Last name is required.'
+        })
+        .nonempty('Last name is required.'),
+    contactNumber: z
+        .string({
+            required_error: 'Contact number is required.',
+            invalid_type_error: 'Contact number is required.'
+        })
+        .nonempty('Contact number is required.')
+        .refine(
+            validator.isMobilePhone,
+            'Please enter a valid contact number.'
+        ),
+    email: z
+        .string({
+            required_error: 'Email address is required.',
+            invalid_type_error: 'Email address is required.'
+        })
+        .nonempty('Email address is required.')
+        .email('Please enter a valid email address.'),
+    query: z
+        .string({
+            required_error: 'Query message is required.',
+            invalid_type_error: 'Query message is required.'
+        })
+        .nonempty('Query message is required.')
+});
+
+export type schemaType = z.infer<typeof schema>;
+
 const ContactUs = () => {
-    const handleSubmitForm = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-    };
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<schemaType>({
+        resolver: zodResolver(schema)
+    });
+
+    const onSubmit: SubmitHandler<schemaType> = (data: schemaType) =>
+        console.log(data);
 
     const address =
-        'Shubham Complex, Opposite Navrangpura Jain Temple, Navrangpura, Ahmedabad, Gujarat - 380009';
+        'Secure Sure, Shubham Complex, Opposite Navrangpura Jain Temple, Navrangpura, Ahmedabad, Gujarat - 380009';
 
     return (
         <div className="text-[#546FFF] px-[50px] xl:px-[150px] pt-[60px] flex flex-col items-center">
@@ -23,51 +74,83 @@ const ContactUs = () => {
                 <form
                     className="w-1/2 flex flex-col justify-center items-center gap-4 py-10"
                     autoComplete="off"
-                    onSubmit={handleSubmitForm}
+                    onSubmit={handleSubmit(onSubmit)}
                 >
                     <div className="w-full flex gap-4">
-                        <input
-                            type="text"
-                            placeholder="First Name"
-                            name="firstName"
-                            id="firstName"
-                            className="w-1/2 border border-[#546FFF] placeholder-[#546FFF] rounded-full py-2 px-5 outline-none"
-                        />
-                        <input
-                            type="text"
-                            placeholder="Last Name"
-                            name="lastName"
-                            id="lastName"
-                            className="w-1/2 border border-[#546FFF] placeholder-[#546FFF] rounded-full py-2 px-5 outline-none"
-                        />
+                        <div className="w-1/2">
+                            <input
+                                type="text"
+                                placeholder="First Name"
+                                id="firstName"
+                                className="w-full border border-[#546FFF] placeholder-[#546FFF] rounded-full py-2 px-5 outline-none"
+                                {...register('firstName')}
+                            />
+
+                            <p className="text-sm text-red-700">
+                                {_.get(errors, 'firstName.message')}
+                            </p>
+                        </div>
+
+                        <div className="w-1/2">
+                            <input
+                                type="text"
+                                placeholder="Last Name"
+                                id="lastName"
+                                className="w-full border border-[#546FFF] placeholder-[#546FFF] rounded-full py-2 px-5 outline-none"
+                                {...register('lastName')}
+                            />
+
+                            <p className="text-sm text-red-700">
+                                {_.get(errors, 'lastName.message')}
+                            </p>
+                        </div>
                     </div>
 
                     <div className="w-full flex gap-4">
-                        <input
-                            type="email"
-                            placeholder="Email address"
-                            name="email"
-                            id="email"
-                            className="w-1/2 border border-[#546FFF] placeholder-[#546FFF] rounded-full py-2 px-5 outline-none"
-                        />
-                        <input
-                            type="text"
-                            placeholder="Contact number"
-                            name="contactNumber"
-                            id="contactNumber"
-                            className="w-1/2 border border-[#546FFF] placeholder-[#546FFF] rounded-full py-2 px-5 outline-none"
-                        />
+                        <div className="w-1/2">
+                            <input
+                                type="text"
+                                placeholder="Email address"
+                                id="email"
+                                className="w-full border border-[#546FFF] placeholder-[#546FFF] rounded-full py-2 px-5 outline-none"
+                                {...register('email')}
+                            />
+
+                            <p className="text-sm text-red-700">
+                                {_.get(errors, 'email.message')}
+                            </p>
+                        </div>
+
+                        <div className="w-1/2">
+                            <input
+                                type="text"
+                                placeholder="Contact number"
+                                id="contactNumber"
+                                className="w-full border border-[#546FFF] placeholder-[#546FFF] rounded-full py-2 px-5 outline-none"
+                                {...register('contactNumber')}
+                            />
+
+                            <p className="text-sm text-red-700">
+                                {_.get(errors, 'contactNumber.message')}
+                            </p>
+                        </div>
                     </div>
 
-                    <div className="rounded-3xl border border-[#546FFF] bg-white py-3 px-5 w-full">
-                        <textarea
-                            placeholder="Message"
-                            name="message"
-                            id="message"
-                            // cols={30}
-                            className="w-full border-0 placeholder-[#546FFF] outline-none"
-                            rows={5}
-                        />
+                    <div className="w-full">
+                        <div className="rounded-3xl border border-[#546FFF] bg-white py-3 px-5 w-full">
+                            <textarea
+                                placeholder="Query"
+                                id="query"
+                                // cols={30}
+                                className="w-full border-0 placeholder-[#546FFF] outline-none"
+                                rows={5}
+                                {...register('query')}
+                            />
+                        </div>
+
+                        <p className="text-sm text-red-700">
+                            {_.get(errors, 'query.message')}
+                        </p>
                     </div>
 
                     <button
--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -7,112 +7,124 @@ import ManTwo from '../../public/assets/Home/People/2.jpg';
 import ManThree from '../../public/assets/Home/People/3.jpg';
 import ManFour from '../../public/assets/Home/People/4.jpg';
 import HeroVector from '../../public/assets/heroVector.svg';
+import Modal from '../HOC/Modal';
+import { Fragment, useState } from 'react';
 
 const Hero = () => {
+    const [isChatWithUsModalOpen, setIsChatWithUsModalOpen] = useState(false);
+
     return (
-        <div className="flex items-center mt-[90px] gap-[50px] px-[50px] xl:px-[150px]">
-            <div className="w-1/2">
-                <h1 className="font-[700] text-[20px] sm:text-[30px] md:text-[35px] lg:text-[40px] text-[#546FFF]">
-                    We help you find the best Insurance Plan at the right price
-                </h1>
+        <Fragment>
+            <Modal
+                isOpen={isChatWithUsModalOpen}
+                handleModalClose={() => setIsChatWithUsModalOpen(false)}
+            />
 
-                <p className="font-[500] mt-[25px]">
-                    Do you want to protect your future and ensure your peace of
-                    mind? At Secure Sure Insurance Agency, we understand the
-                    importance of safeguarding. We offer comprehensive insurance
-                    services with reliable coverage, the best insurance advice,
-                    and customized solutions tailored to your specific needs.
-                    Whether you need health insurance, term insurance, travel
-                    insurance, or vehicle insurance, you will get all under one
-                    roof.
-                </p>
+            <div className="flex items-center mt-[90px] gap-[50px] px-[50px] xl:px-[150px]">
+                <div className="w-1/2">
+                    <h1 className="font-[700] text-[20px] sm:text-[30px] md:text-[35px] lg:text-[40px] text-[#546FFF]">
+                        We help you find the best Insurance Plan at the right
+                        price
+                    </h1>
+                    <p className="font-[500] mt-[25px]">
+                        Do you want to protect your future and ensure your peace
+                        of mind? At Secure Sure Insurance Agency, we understand
+                        the importance of safeguarding. We offer comprehensive
+                        insurance services with reliable coverage, the best
+                        insurance advice, and customized solutions tailored to
+                        your specific needs. Whether you need health insurance,
+                        term insurance, travel insurance, or vehicle insurance,
+                        you will get all under one roof.
+                    </p>
+                    <div className="flex gap-[25px] my-[40px]">
+                        <div
+                            className="bg-[#546FFF] py-[10px] px-[35px] rounded-[100px] text-white cursor-pointer"
+                            onClick={() => setIsChatWithUsModalOpen(true)}
+                        >
+                            <p>Chat with us</p>
+                        </div>
 
-                <div className="flex gap-[25px] my-[40px]">
-                    <div className="bg-[#546FFF] py-[10px] px-[35px] rounded-[100px] text-white cursor-pointer">
-                        <p>Chat with us</p>
+                        <div className="relative">
+                            <Link href="/services">
+                                <div className="bg-[#E1FFF1] border border-[#309C68] text-[#309C68] py-[10px] px-[35px] rounded-[100px]">
+                                    Explore services
+                                </div>
+                            </Link>
+
+                            <Image
+                                src={One}
+                                alt="Explore services"
+                                width={20}
+                                className="absolute bottom-[-15px] right-[-11px]"
+                            />
+                        </div>
                     </div>
+                    <div className="flex items-center gap-4">
+                        <div className="flex">
+                            <span className="rounded-[50%] relative inline-block overflow-hidden border-4 border-white">
+                                <Image
+                                    src={ManOne}
+                                    alt="Happy Customer"
+                                    quality={80}
+                                    width={55}
+                                />
+                            </span>
 
-                    <div className="relative">
-                        <Link href="/services">
-                            <div className="bg-[#E1FFF1] border border-[#309C68] text-[#309C68] py-[10px] px-[35px] rounded-[100px]">
-                                Explore services
-                            </div>
-                        </Link>
+                            <span className="rounded-[50%] relative inline-block overflow-hidden ml-[-30px] border-4 border-white">
+                                <Image
+                                    src={ManTwo}
+                                    alt="Happy Customer"
+                                    quality={80}
+                                    width={55}
+                                />
+                            </span>
 
-                        <Image
-                            src={One}
-                            alt="Explore services"
-                            width={20}
-                            className="absolute bottom-[-15px] right-[-11px]"
-                        />
+                            <span className="rounded-[50%] relative inline-block overflow-hidden ml-[-30px] border-4 border-white">
+                                <Image
+                                    src={ManThree}
+                                    alt="Happy Customer"
+                                    quality={80}
+                                    width={55}
+                                />
+                            </span>
+
+                            <span className="rounded-[50%] relative inline-block overflow-hidden ml-[-30px] border-4 border-white">
+                                <Image
+                                    src={ManFour}
+                                    alt="Happy Customer"
+                                    quality={80}
+                                    width={55}
+                                />
+                            </span>
+                        </div>
+
+                        <div className="relative">
+                            <p className="text-[#309C68] underline">
+                                2000+ satisfied and happy customers
+                            </p>
+
+                            <Image
+                                src={Two}
+                                alt="Explore services"
+                                width={20}
+                                className="absolute bottom-[-16px] right-[-18px]"
+                            />
+                        </div>
                     </div>
                 </div>
 
-                <div className="flex items-center gap-4">
-                    <div className="flex">
-                        <span className="rounded-[50%] relative inline-block overflow-hidden border-4 border-white">
-                            <Image
-                                src={ManOne}
-                                alt="Happy Customer"
-                                quality={80}
-                                width={55}
-                            />
-                        </span>
-
-                        <span className="rounded-[50%] relative inline-block overflow-hidden ml-[-30px] border-4 border-white">
-                            <Image
-                                src={ManTwo}
-                                alt="Happy Customer"
-                                quality={80}
-                                width={55}
-                            />
-                        </span>
-
-                        <span className="rounded-[50%] relative inline-block overflow-hidden ml-[-30px] border-4 border-white">
-                            <Image
-                                src={ManThree}
-                                alt="Happy Customer"
-                                quality={80}
-                                width={55}
-                            />
-                        </span>
-
-                        <span className="rounded-[50%] relative inline-block overflow-hidden ml-[-30px] border-4 border-white">
-                            <Image
-                                src={ManFour}
-                                alt="Happy Customer"
-                                quality={80}
-                                width={55}
-                            />
-                        </span>
-                    </div>
-
-                    <div className="relative">
-                        <p className="text-[#309C68] underline">
-                            2000+ satisfied and happy customers
-                        </p>
-
-                        <Image
-                            src={Two}
-                            alt="Explore services"
-                            width={20}
-                            className="absolute bottom-[-16px] right-[-18px]"
-                        />
-                    </div>
+                <div className="flex-1">
+                    <Image
+                        src={HeroVector}
+                        alt="Your family is secured."
+                        width={680}
+                        height={600}
+                        className="m-auto"
+                        priority
+                    />
                 </div>
             </div>
-
-            <div className="flex-1">
-                <Image
-                    src={HeroVector}
-                    alt="Your family is secured."
-                    width={680}
-                    height={600}
-                    className="m-auto"
-                    priority
-                />
-            </div>
-        </div>
+        </Fragment>
     );
 };
 
